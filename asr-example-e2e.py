a577c32880e1bd4146cd8b16ce63374c2e8aea74
DogHeadMoon <mathewdu1314@gmail.com>
2022-04-07 09:25:19 +0800

    update e2e

---
+++ asr-example-e2e.py
@@ -0,0 +1,92 @@
+import collections
+import numpy as np
+import os
+import time
+import base64
+import json
+from scipy.io import wavfile
+import requests
+
+from concurrent.futures import ThreadPoolExecutor
+import threading
+
+#ASR_URL = 'http://record-asr.lizhifm.com'
+#ASR_URL = 'http://0.0.0.0:10086'
+#ASR_URL = 'http://127.0.0.1:10085'
+ASR_URL = 'http://localhost:10086'
+#ASR_URL = 'http://127.0.0.1:10087'
+
+def read_list(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        f.close()
+        rt = [item.strip() for item in lines]
+        return rt
+
+def post_one(path):
+    name = os.path.basename(path)
+    with open(path, 'rb') as f:
+        content = f.read()
+        f.close()
+        encoded_str = base64.b64encode(content)
+        basee64_file = encoded_str.decode('utf-8')
+        body = {'audioBase64': basee64_file, 'lang': 1, 'scene': 0, 'aue': 'm4a', 'id': name}
+
+        # 发起请求
+        st = time.time()
+        try:
+            r=httpx.post(ASR_URL, json=body, timeout=20)
+            if r.status_code != 200:
+                print('{}\trequests fails code:{} details:{}'.format(name, r.status_code, r.json()))
+            else:
+                #print('result: {}'.format(r.json()['result']))
+                end = time.time()
+                consume = end - st
+                text = r.json()['result']
+                print('{}\t{}\t{:.3f}'.format(name, text, consume))
+        except Exception as e:
+                print(e)
+        except httpx.ReadTimeout:
+            print('{}\tError {}'.format(name, 'httpx.readtimeout'))
+
+def post_list(paths):
+    for path in paths:
+        post_one(path)
+
+def split_list(paths, num):
+    n = len(paths)
+    each = int((n+1)/num)
+    rt = []
+    for i in range(num-1):
+        rt.append(paths[each*i: each*(i+1)])
+    rt.append(paths[(num-1)*each:])
+    return rt
+
+if __name__ == '__main__':
+    path = '/DATA/disk1/duyao/workplace/e2e-release/record1.wav'
+    name = os.path.basename(path)
+    with open(path, 'rb') as f:
+        content = f.read()
+        f.close()
+        encoded_str = base64.b64encode(content)
+        basee64_file = encoded_str.decode('utf-8')
+
+    #aue set to wav or mp3 or m4a
+    #body = {'audioBase64': basee64_file, 'lang': 1, 'scene': 0, 'aue': 'wav', 'id': name}
+    #body = {'audioBase64': basee64_file, 'lang': 1, 'scene': 0, 'aue': 'wav', 'id': name, 'custom_words':'李长亮'}
+    body = {'audioBase64': basee64_file, 'lang': 1, 'scene': 0, 'aue': 'wav', 'id': name}
+
+    # 发起请求
+    st = time.time()
+    r = requests.post(ASR_URL, json=body, timeout=300)
+    if r.status_code != 200:
+        print('requests fails code:{}'.format(r.status_code))
+    else:
+        end = time.time()
+        consume = end - st
+        #print(r.json())
+        text = r.json()['result']
+        timestamps = r.json()['timestamps']
+        #for i in range(len(sentences)):
+        #  print(type(sentences[i]), sentences[i])
+        print('{}\t{}\t{:.3f}'.format(name, text, consume))
--- e2e.py
+++ e2e.py
@@ -26,6 +26,7 @@ from offline_client import SpeechClient
 import tritonclient.grpc as grpcclient
 from collections import defaultdict
 
+import websockets
 import logging
 from logging import FileHandler
 
@@ -34,34 +35,29 @@ app = Flask(__name__)
 url='localhost:8001'
 id=1
 
-
-
-def concat_parts(paths, dic):
+def concat_parts(paths, dic, boundaries):
+  val_texts = []
+  val_boundaries = []
   rt = ''
-  for path in paths:
+  for i,path in enumerate(paths):
     if path in dic.keys():
       part_rt = dic[path] 
       if len(part_rt) > 2 :
-        print(len(part_rt), part_rt)
-        rt +=  part_rt + '，'
+        val_texts.append(part_rt)
+        val_boundaries.append(boundaries[i])
     else:
       app.logger.error('error : {} not in result keys'.format(path))
-  if len(rt) > 0:
-    if rt[-1] == '，':
-      rt=rt[:-1]
-  return rt
+  return val_texts, val_boundaries
 
 def single_job(client_files):
     rt = {}
     with grpcclient.InferenceServerClient(url=url,
-                                          verbose=False) as triton_client:
+                                          verbose=False) as client:
         protocol_client = grpcclient
-        speech_client = SpeechClient(triton_client, 'attention_rescoring', protocol_client)
+        speech_client = SpeechClient(client, 'attention_rescoring', protocol_client)
         idx, audio_files = client_files
-        #predictions = []
         for li in audio_files:
             result = speech_client.recognize(li, idx) 
-            #predictions += result
             rt[li] = result[0]
     return rt 
 
@@ -80,11 +76,112 @@ def export_parts(name, pcm_parts):
     new_paths.append(new_path)
   return new_paths
   
-#@app.route('/lizhi-asr', methods=['GET', 'POST'])
+
+def get_st_msg(custom_words):
+  message = {}
+  message['signal'] = 'start'
+  message['nbest'] = 1
+  message['continuous_decoding'] = False
+  if len(custom_words) > 0:
+    message['custom_words'] = custom_words
+  msg = json.dumps(message)
+  return msg
+
+def get_end_msg():
+  end_msg={'signal':'end'}
+  msg = json.dumps(end_msg)
+  return msg
+
+async def post_cpu(pcm, name, custom_words):
+  try:
+    async with websockets.connect('ws://localhost:10089') as websocket:
+        await websocket.send(get_st_msg(custom_words))
+        rsp_js = await websocket.recv()
+        rsp = json.loads(rsp_js)
+        if 'status' in rsp.keys():
+          if rsp['status'] == 'ok':
+            await websocket.send(pcm)
+            await websocket.send(get_end_msg())
+            rt = await websocket.recv()
+            print("name : {}, rt : {}".format(name, rt))
+            final = json.loads(rt)
+            print("after final")
+            text = final['nbest']
+            if websocket.open:
+              await websocket.close()
+            return (name, text)
+  except Exception as e:
+    return (name, text)
+
+def post_multi_cpu(name, pcms, custom_words, boundaries):
+  n = len(pcms)
+  #loop = asyncio.get_event_loop()
+  loop1 = asyncio.new_event_loop()
+  asyncio.set_event_loop(loop1)
+  loop = asyncio.get_event_loop()
+  batch=100
+  nfulls = n//batch
+  nresidue = n%batch
+  names = []
+  val_rt = []
+  rt_dic = {}
+  val_boundaries = []
+
+  if nfulls:
+    for i in range(nfulls):
+      tasks = []
+      for j in batch:
+        new_name = "{}-{}".foramt(name, i*batch + j)
+        names.append(new_name)
+        tasks.append(post_cpu(pcms[i*batch + j], new_name))
+      done, pending = loop.run_until_complete(asyncio.wait(tasks, timeout=100))
+      print("done", done)
+      for fut in done:
+        items = fut.result()
+        print(items)
+        if len(items) == 2:
+          key = items[0]
+          text = items[1]
+          rt_dic[key] = text
+  if nresidue:
+    tasks = []
+    for j in range(nresidue):
+      new_name = "{}-{}".format(name, nfulls*batch + j)
+      names.append(new_name)
+      tasks.append(post_cpu(pcms[nfulls*batch + j], new_name, custom_words))
+    done, pending = loop.run_until_complete(asyncio.wait(tasks, timeout=100))
+    print("done", done)
+    for fut in done:
+      items = fut.result()
+      print(items)
+      if len(items) == 2:
+        key = items[0]
+        text = items[1]
+        rt_dic[key] = text
+  
+  for i,key in enumerate(names):
+    if key in rt_dic.keys():
+      val_boundaries.append(boundaries[i])
+      val_rt.append(rt_dic[key])
+  return val_rt, val_boundaries
+
+def get_timestamps_json(texts, boundaries):
+  n=min(len(texts), len(boundaries))
+  segs = []
+  for i in range(n):
+    seg={}
+    seg['sentence']=texts[i]
+    seg['start']=boundaries[i][0]
+    seg['end']=boundaries[i][1]
+    segs.append(seg)
+  return segs
+  
+
 @app.route('/', methods=['GET', 'POST'])
 def process():
     rt = ''
-    app.logger.error('request method : {}'.format(request.method))
+    timestamps = []
+    #app.logger.info('request method : {}'.format(request.method))
     if request.method == 'POST':
       if request.is_json:
         json_data = request.json
@@ -93,6 +190,10 @@ def process():
         aue = json_data['aue']
         name = json_data['id']
         n = len(audio_bytes)
+        custom_words =''
+        if 'custom_words' in json_data.keys():
+          custom_words = json_data['custom_words']
+          print('custom_words : ', custom_words)
 
         content = None
         if aue == 'm4a':
@@ -113,7 +214,6 @@ def process():
         elif aue == 'pcm':
             ts=datetime.datetime.now().strftime("%m-%d_%H-%M-%S_%f")
             new_path='inhouse-parts/{}_{}.pcm'.format(ts, name)
-            print(new_path)
             buff = io.BytesIO(audio_bytes)  
             AudioSegment.from_raw(buff, sample_width=2, frame_rate=16000, channels=1).export(new_path, format='wav')
             buff.close()
@@ -130,25 +230,33 @@ def process():
             content = audio_bytes
         else:
             return
+        
         avad=Vad()
-        vad_st = time.time()
-        parts = avad.get_parts(content)
-        vad_end = time.time()
-        #contents.append(content)
-        print('before export parts, name {}'.format(name))
-        paths=export_parts(name, parts)
-        print('after export parts')
-        for i in range(len(parts)):
-          print('part {} len : {}'.format(i, len(parts[i])))
-    
-        global id
-        if len(paths) > 0: 
-          dic_rt = single_job((id, paths))
-          rt = concat_parts(paths, dic_rt)
-          id+=1
-          app.logger.info("{}\t{}".format(name, rt))
-    js = {'result':rt}
+        parts, boundaries = avad.get_parts(content)
+
+        if len(parts):
+          if len(custom_words):
+            texts, boundaries = post_multi_cpu(name, parts, custom_words, boundaries)
+            timestamps = get_timestamps_json(texts, boundaries)
+          else:
+            paths=export_parts(name, parts)
+            global id
+            if len(paths): 
+              dic_rt = single_job((id, paths))
+              texts, boundaries = concat_parts(paths, dic_rt, boundaries)
+              timestamps = get_timestamps_json(texts, boundaries)
+              id+=1
+        if len(texts):
+          rt = '，'.join(texts)
+          rt += '。'
+
+        #print('after export parts')
+        #for i in range(len(parts)):
+        #  print('part {} len : {}'.format(i, len(parts[i])))
+        app.logger.info("{}\t{}".format(name, rt))
+    js = {'result':rt, 'timestamps':timestamps}
     js_rt=json.dumps(js)
+    print(js_rt)
     return js_rt
 
 if __name__ == '__main__':
